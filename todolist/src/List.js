609e85e4309ba50026d5a4aa68f9349283478c46
janette <janettewongprasith@gmail.com>
2021-03-26 21:26:12 +0700

    last todo-list

--- todolist/src/List.js
+++ todolist/src/List.js
@@ -1,5 +1,7 @@
-import {useState} from 'react';
+import { useState } from "react";
 import { Card } from "./components/Card/Card";
+import Header from "./components/Header/Header";
+import Headersignup from "./components/Header/Headersignup";
 import { InputBox } from "./components/InputBox/InputBox";
 import { TodosBox } from "./components/Todos/TodosBox/TodosBox";
 const List = () => {
@@ -8,30 +10,33 @@ const List = () => {
   //   {id:2, text:"efg"},
   //   {id:3, text:"hij"}
   // ];
-  const [todos,setTodos] = useState([
-    {id:1, text:"Go shopping"},
-    {id:2, text:"Do homework"},
-    {id:3, text:"Clean my room"}
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Go shopping" },
+    { id: 2, text: "Do homework" },
+    { id: 3, text: "Clean my room" },
   ]);
 
-  const onAddHandler= (text) => {
-    setTodos([...todos, {id:Math.random(),text:text}]);
+  const onAddHandler = (text) => {
+    setTodos([...todos, { id: Math.random(), text: text }]);
   };
 
   const onDeleteHandler = (id) => {
-    const newTodo = todos.filter((todo) => todo.id !== id)
-    setTodos(newTodo)
+    const newTodo = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodo);
   };
 
-    return (
+  return (
+    <div>
+      <Headersignup />
         <div className="listbox">
-             <h1 className="header-text">TO-DO LIST</h1>
-      <Card>
-        <InputBox onAdd={onAddHandler}/>
-        <TodosBox todos={todos} onDelete={onDeleteHandler}/>
-      </Card>
+          <h1 className="header-text">TO-DO LIST</h1>
+          <Card>
+            <InputBox onAdd={onAddHandler} />
+            <TodosBox todos={todos} onDelete={onDeleteHandler} />
+          </Card>
         </div>
-    )
-}
+    </div>
+  );
+};
 
 export default List;
